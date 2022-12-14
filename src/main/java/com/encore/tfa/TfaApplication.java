3803e41ba9ab0a20bfa712fc029df5116c900d33
Kimsunfang <kimsunfang@gmail.com>
2022-12-14 15:29:30 +0900

    Feat:Create chatting#12

--- src/main/java/com/encore/tfa/TfaApplication.java
+++ src/main/java/com/encore/tfa/TfaApplication.java
@@ -2,12 +2,22 @@ package com.encore.tfa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class TfaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TfaApplication.class, args);
+	}
+
+	@Controller
+	public static class MainController {
+		@GetMapping("/")
+		public String main() {
+			return "redirect:/chat/rooms";
+		}
 	}
 
 }
