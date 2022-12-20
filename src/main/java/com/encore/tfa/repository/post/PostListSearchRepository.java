61fa1d3b242eafbd559f23d262c5408e778fdbf2
kgh3489 <kgh3489@naver.com>
2022-12-20 15:32:25 +0900

    Fix:Paganation error #24

---
+++ src/main/java/com/encore/tfa/repository/post/PostListSearchRepository.java
@@ -0,0 +1,15 @@
+package com.encore.tfa.repository.post;
+
+import com.encore.tfa.controller.post.response.PostWithUserResponse;
+import com.encore.tfa.model.post.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostListSearchRepository extends JpaRepository<Post, Long> {
+//    Page<PostWithUserResponse> getPostListPage(Long userId, Pageable pageable);
+    Page<PostWithUserResponse> getPostByUserId(Long userId, Pageable pageable);
+
+}
---
+++ src/main/java/com/encore/tfa/service/post/PostGetListService.java
@@ -0,0 +1,44 @@
+package com.encore.tfa.service.post;
+
+import com.encore.tfa.controller.post.request.PostListGetByUserIdRequest;
+import com.encore.tfa.controller.post.response.PostListGetByUserIdResponse;
+import com.encore.tfa.controller.post.response.PostWithUserResponse;
+import com.encore.tfa.exception.NonExistResourceException;
+import com.encore.tfa.model.user.User;
+import com.encore.tfa.repository.UserRepository;
+import com.encore.tfa.repository.post.PostListSearchRepository;
+import com.encore.tfa.util.mapper.PostGetListMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostGetListService {
+
+    private final PostListSearchRepository postListSearchRepository;
+    private final UserRepository userRepository;
+
+    public PostGetListService(PostListSearchRepository postListSearchRepository, UserRepository userRepository) {
+        this.postListSearchRepository = postListSearchRepository;
+        this.userRepository = userRepository;
+    }
+
+    public PostListGetByUserIdResponse getPostListLByUserId(
+            PostListGetByUserIdRequest postListGetByUserIdRequest){
+        User user = userRepository.findById(postListGetByUserIdRequest.getUserId())
+                .orElseThrow(()->new NonExistResourceException("User could not be fouhnd"));
+
+//        Page<PostWithUserResponse> postPage = postListSearchRepository.getPostListPage(user.getId(),
+//                convertPageRequest(postListGetByUserIdRequest));
+        Page<PostWithUserResponse> postPage = postListSearchRepository.getPostByUserId(user.getId(),
+                convertPageRequest(postListGetByUserIdRequest));
+        return PostGetListMapper.of().postListToResponse(postPage);
+    }
+
+    private static PageRequest convertPageRequest(PostListGetByUserIdRequest request){
+        return PageRequest.of(request.getPageNum() - 1,
+                request.getAmount());
+    }
+
+
+}
