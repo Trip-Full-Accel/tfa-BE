91c832f0fe87c46a39535482b42107b7de942972
kgh3489 <kgh3489@naver.com>
2022-12-27 16:40:08 +0900

    Fix:Modify course response #32

--- src/main/java/com/encore/tfa/controller/course/response/CourseResponse.java
+++ src/main/java/com/encore/tfa/controller/course/response/CourseResponse.java
@@ -1,20 +1,27 @@
 package com.encore.tfa.controller.course.response;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
+import java.util.List;
 import java.util.Map;
 
 @Getter
 public class CourseResponse {
 
-    private final Map<Integer, String> courseNames;
+    private Integer courseOrders;
 
-    private final Double[] courseLats;
+    private String courseNames;
 
-    private final Double[] courseLngs;
+    private Double courseLats;
 
-    public CourseResponse(Map<Integer, String> courseNames, Double[] courseLats, Double[] courseLngs) {
+    private Double courseLngs;
+
+    public CourseResponse(Integer courseOrders, String courseNames, Double courseLats, Double courseLngs) {
+        this.courseOrders = courseOrders;
         this.courseNames = courseNames;
         this.courseLats = courseLats;
         this.courseLngs = courseLngs;
